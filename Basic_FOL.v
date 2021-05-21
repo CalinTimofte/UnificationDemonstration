352a75479b36fde6bfedc0170da83633526e6d0a
Calin Timofte <calin.timofte@yahoo.com>
2021-05-22 00:10:04 +0300

    Fixes func def and adds fake_parse to work with formulas

--- Basic_FOL.v
+++ Basic_FOL.v
@@ -27,23 +27,29 @@ Check ([named_var "a"; named_var "b"]).
 Definition a := named_var "a".
 Definition b := named_var "b".
 
-Inductive functional_symbol : Type :=
-  | constant : string -> functional_symbol
-  | func : string -> list var -> functional_symbol.
+Inductive func : string -> list var -> Type :=
+  | constant_symbol (name : string): func name []
+  | functional_symbol (name : string) (vars : list var): func name vars.
+
+Definition fake_parse_func (f : Type) : var := named_var "default".
 
 Inductive predicate : string -> list var -> Prop:=
   | predicative_symbol (name : string)  (vars : list var) : predicate name vars.
 
-Check (func "f" [a; b]).
+Check (functional_symbol "f" [a; b]).
 Definition f := func "f".
 Check (f [a;b]).
 Check (f).
+Compute (fake_parse_func (f[a])).
 
+Check (predicate).
 Check (predicate "P" [a]).
 Definition P := predicate "P".
 Check (P [a]).
 
 Check ( forall x : var, P [x]).
+
+Check (forall x:var, (forall y:var, ((P [x; y]) -> exists z:var, ((P [x; z]) /\ (P [z;y]))))).
 
 
 
