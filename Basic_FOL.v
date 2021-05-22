fd8aa04887a1bca65fab174d76c7f05d11cd8e18
Calin Timofte <calin.timofte@yahoo.com>
2021-05-23 00:17:38 +0300

    Rewrites term, separating symbol from functionality types, perhaps ill formed still. Adds atomic_formulae.

--- Basic_FOL.v
+++ Basic_FOL.v
@@ -4,18 +4,6 @@ Open Scope string_scope.
 Require Import List.
 Import ListNotations.
 
-(*Inductive var: Type.
-Variable P: list var -> functional_symbol.
-Definition vars (f : functional_symbol) : list var :=
-  match f with
-  | constant => []
-  | func l => l
-  end.
-Definition arity (f : functional_symbol) : nat :=
-  length (vars f).
-Compute (vars (func [a;b])).
-Compute (arity (func [a;b;c])). *)
-
 Inductive var: Type :=
   | named_var: string -> var.
 
@@ -23,38 +11,16 @@ Check ([named_var "a"; named_var "b"]).
 Definition a := named_var "a".
 Definition b := named_var "b".
 
-Inductive const : Type :=
-  | cconst: string -> const.
+Inductive functional_symbol : Type :=
+  | func: string -> functional_symbol.
 
-Inductive func : Type :=
-  | fconst: const -> func
-  | ffunc: string -> list var_or_term -> func
+Inductive predicative_symbol : Type :=
+  | predicate: string -> predicative_symbol.
 
-with var_or_term : Type :=
-  | v : var -> var_or_term
-  | t : term -> var_or_term
+Inductive term : Type :=
+  | tconst : functional_symbol -> term
+  | tvar : var -> term
+  | tfunc : functional_symbol -> list term -> term.
 
-with term : Type :=
-  | tconst: const -> term
-  | tvar: var -> term
-  | tfunc: func -> term.
-
-Definition fake_parse_func (f : func) : var := named_var "default".
-
-Inductive predicate : string -> list var -> Prop:=
-  | predicative_symbol (name : string)  (vars : list var) : predicate name vars.
-
-Check (ffunc "f" [v a; v b]).
-Definition f := ffunc "f".
-Check (f [v a;v b]).
-Check (f).
-Compute (fake_parse_func (f[v a])).
-
-Check (predicate).
-Check (predicate "P" [a]).
-Definition P := predicate "P".
-Check (P [a]).
-
-Check ( forall x : var, P [x]).
-
-Check (forall x:var, (forall y:var, ((P [x; y]) -> exists z:var, ((P [x; z]) /\ (P [z;y]))))).
+Inductive atomic_formulae : Type :=
+  | afpred : predicative_symbol -> list term -> atomic_formulae.
