27f2e85d4e9fffb33acc7c0d42f9939c4645fc7e
Calin Timofte <calin.timofte@yahoo.com>
2021-05-23 00:01:40 +0300

    Defines term related to function, possibly ill-defined.

--- Basic_FOL.v
+++ Basic_FOL.v
@@ -5,18 +5,14 @@ Require Import List.
 Import ListNotations.
 
 (*Inductive var: Type.
-
 Variable P: list var -> functional_symbol.
-
 Definition vars (f : functional_symbol) : list var :=
   match f with
   | constant => []
   | func l => l
   end.
-
 Definition arity (f : functional_symbol) : nat :=
   length (vars f).
-
 Compute (vars (func [a;b])).
 Compute (arity (func [a;b;c])). *)
 
@@ -27,20 +23,32 @@ Check ([named_var "a"; named_var "b"]).
 Definition a := named_var "a".
 Definition b := named_var "b".
 
-Inductive func : string -> list var -> Type :=
-  | constant_symbol (name : string): func name []
-  | functional_symbol (name : string) (vars : list var): func name vars.
+Inductive const : Type :=
+  | cconst: string -> const.
 
-Definition fake_parse_func (f : Type) : var := named_var "default".
+Inductive func : Type :=
+  | fconst: const -> func
+  | ffunc: string -> list var_or_term -> func
+
+with var_or_term : Type :=
+  | v : var -> var_or_term
+  | t : term -> var_or_term
+
+with term : Type :=
+  | tconst: const -> term
+  | tvar: var -> term
+  | tfunc: func -> term.
+
+Definition fake_parse_func (f : func) : var := named_var "default".
 
 Inductive predicate : string -> list var -> Prop:=
   | predicative_symbol (name : string)  (vars : list var) : predicate name vars.
 
-Check (functional_symbol "f" [a; b]).
-Definition f := func "f".
-Check (f [a;b]).
+Check (ffunc "f" [v a; v b]).
+Definition f := ffunc "f".
+Check (f [v a;v b]).
 Check (f).
-Compute (fake_parse_func (f[a])).
+Compute (fake_parse_func (f[v a])).
 
 Check (predicate).
 Check (predicate "P" [a]).
@@ -50,6 +58,3 @@ Check (P [a]).
 Check ( forall x : var, P [x]).
 
 Check (forall x:var, (forall y:var, ((P [x; y]) -> exists z:var, ((P [x; z]) /\ (P [z;y]))))).
-
-
-
