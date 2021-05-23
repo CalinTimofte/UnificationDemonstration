dc33cedf00f122835f7df513f9538c22300b819e
Calin Timofte <calin.timofte@yahoo.com>
2021-05-24 00:29:21 +0300

    Runs test, definite ill-defined fol formulae idk how to fix yet

--- Basic_FOL.v
+++ Basic_FOL.v
@@ -8,6 +8,7 @@ Inductive var: Type :=
   | named_var: string -> var.
 
 Check ([named_var "a"; named_var "b"]).
+Check(named_var "a").
 Definition a := named_var "a".
 Definition b := named_var "b".
 
@@ -22,13 +23,16 @@ Inductive term : Type :=
   | tvar : var -> term
   | tfunc : functional_symbol -> list term -> term.
 
-Inductive atomic_formulae : Prop :=
-  | afpred: predicative_symbol -> list term -> atomic_formulae.
+Inductive atomic_formulae : predicative_symbol -> list term -> Prop :=
+  | afpred (p : predicative_symbol) (l : list term): atomic_formulae p l.
 
 Check (afpred (predicate "P") [tvar a]).
+Definition phi := atomic_formulae (predicate "p") [tvar a].
+Check (phi /\ phi).
+
 
 Inductive first_order_formulae : Type :=
-  | Aformula (p : atomic_formulae)
+  | Aformula (p: predicative_symbol)(l: list term)
   | Anot (phi : first_order_formulae)
   | Aand (phi1 phi2 : first_order_formulae)
   | Aor (phi1 phi2 : first_order_formulae)
@@ -39,5 +43,30 @@ Inductive first_order_formulae : Type :=
 
 Fixpoint first_order_formulae_eval (phi: first_order_formulae) : Prop :=
   match phi with
-  | Aformula phi0 => phi0
+  | Aformula p l => atomic_formulae p l
+  | Anot phi0 => ~(first_order_formulae_eval phi0)
+  | Aand phi1 phi2 => (first_order_formulae_eval phi1) /\ (first_order_formulae_eval phi2)
+  | Aor phi1 phi2 => (first_order_formulae_eval phi1) \/ (first_order_formulae_eval phi2)
+  | Aimplies phi1 phi2 => (first_order_formulae_eval phi1) -> (first_order_formulae_eval phi2)
+  | Adoubleimplies phi1 phi2 => (first_order_formulae_eval phi1) <-> (first_order_formulae_eval phi2)
+  | Aforall x phi0 => forall x: var, (first_order_formulae_eval phi0)
+  | Aexists x phi0 => exists x: var, (first_order_formulae_eval phi0)
   end.
+
+(* Run some tests *)
+
+Definition P := predicate "P".
+Check(Aformula P [tvar a]).
+Definition phi1 := Aformula P [tvar a].
+Definition phi2 := Aformula P [tvar b; tvar b].
+Check(Aand phi1 phi2).
+Check(Aexists a phi1).
+
+Compute first_order_formulae_eval phi1.
+Compute first_order_formulae_eval (Anot phi1).
+Compute first_order_formulae_eval (Aand phi1 phi2).
+Compute first_order_formulae_eval (Aor phi1 phi2).
+Compute first_order_formulae_eval (Aimplies phi1 phi2).
+Compute first_order_formulae_eval (Adoubleimplies phi1 phi2).
+Compute first_order_formulae_eval (Aforall a phi1).
+Compute first_order_formulae_eval (Aexists b phi2).
