04dcf1925820aacf2b2327221366bd918e75c270
Calin Timofte <calin.timofte@yahoo.com>
2021-05-24 17:00:16 +0300

    Removes duplicates from var list

--- Basic_FOL.v
+++ Basic_FOL.v
@@ -23,7 +23,7 @@ Inductive term : Type :=
   | Tvar : var -> term
   | Tfunc : functional_symbol -> list term -> term.
 
-Inductive atomic_formulae : Prop :=
+Inductive atomic_formulae : Type :=
   | Afpred: predicative_symbol -> list term -> atomic_formulae.
 
 Check (Afpred (Predicate "P") [Tvar a]).
@@ -43,13 +43,56 @@ Inductive first_order_formulae : Type :=
 Compute(hd a [b;a]).
 Compute (hd (Tvar a) [Tvar b; Tvar a]).
 
+Definition var_get_name (v : var) : string := 
+  match v with
+  | Named_var s => s
+  end.
+
+Check (Named_var "a" = Named_var "b").
+
+Fixpoint disassemble_var_list (l : list var) : list string :=
+  match l with
+  | [] => []
+  | h::t => [var_get_name h] ++ disassemble_var_list t
+  end.
+
+Fixpoint assemble_var_list (l : list string) : list var :=
+  match l with
+  | [] => []
+  | h::t => [Named_var h] ++ assemble_var_list t
+  end.
+
+Compute (disassemble_var_list [Named_var "a"; Named_var "b"; Named_var "b"]).
+Compute (assemble_var_list ["a"; "b"; "b"]).
+
+Definition remove_duplicates_var_list (l : list var) : list var :=
+  assemble_var_list (nodup string_dec (disassemble_var_list l)).
+
+Compute (remove_duplicates_var_list [Named_var "a"; Named_var "b"; Named_var "b"]).
 
 Fixpoint vars_term (t : term) : list var :=
   match t with
   | Tconst c => []
   | Tvar v => [v]
-  | Tfunc f l => flat_map vars_term l
+  | Tfunc f l => remove_duplicates_var_list (flat_map vars_term l)
   end.
 
-Compute (vars_term (Tfunc (Func "f") [(Tvar a); (Tfunc (Func "g") [Tvar b; Tvar b])])).
+Definition vars_atomic_formulae (a : atomic_formulae) : list var :=
+  match a with
+  | Afpred p l => flat_map vars_term l
+  end.
 
+Fixpoint vars_first_order_formulae (phi : first_order_formulae) : list var :=
+  match phi with
+  | Aformula phi0 => vars_atomic_formulae phi0
+  | Anot phi0 => vars_first_order_formulae phi0
+  | Aand phi1 phi2 => (vars_first_order_formulae phi1) ++ (vars_first_order_formulae phi2)
+  | Aor phi1 phi2 => (vars_first_order_formulae phi1) ++ (vars_first_order_formulae phi2)
+  | Aimplies phi1 phi2 => (vars_first_order_formulae phi1) ++ (vars_first_order_formulae phi2)
+  | Adoubleimplies phi1 phi2 => (vars_first_order_formulae phi1) ++ (vars_first_order_formulae phi2)
+  | Aforall x phi0 => (vars_first_order_formulae phi0) ++ [x]
+  | Aexists x phi0 => (vars_first_order_formulae phi0) ++ [x]
+  end.
+
+
+Compute (vars_term (Tfunc (Func "f") [(Tvar a); (Tfunc (Func "g") [Tvar b; Tvar b])])).
