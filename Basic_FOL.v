fbe18341b0956ec434788e977087cb42f43f77ce
Calin Timofte <calin.timofte@yahoo.com>
2021-05-24 12:38:55 +0300

    Removes some code, focusing just on synta now, might add home-brew props

--- Basic_FOL.v
+++ Basic_FOL.v
@@ -5,34 +5,33 @@ Require Import List.
 Import ListNotations.
 
 Inductive var: Type :=
-  | named_var: string -> var.
+  | Named_var: string -> var.
 
-Check ([named_var "a"; named_var "b"]).
-Check(named_var "a").
-Definition a := named_var "a".
-Definition b := named_var "b".
+Check ([Named_var "a"; Named_var "b"]).
+Check(Named_var "a").
+Definition a := Named_var "a".
+Definition b := Named_var "b".
 
 Inductive functional_symbol : Type :=
-  | func: string -> functional_symbol.
+  | Func: string -> functional_symbol.
 
 Inductive predicative_symbol : Type :=
-  | predicate: string -> predicative_symbol.
+  | Predicate: string -> predicative_symbol.
 
 Inductive term : Type :=
-  | tconst : functional_symbol -> term
-  | tvar : var -> term
-  | tfunc : functional_symbol -> list term -> term.
+  | Tconst : functional_symbol -> term
+  | Tvar : var -> term
+  | Tfunc : functional_symbol -> list term -> term.
 
-Inductive atomic_formulae : predicative_symbol -> list term -> Prop :=
-  | afpred (p : predicative_symbol) (l : list term): atomic_formulae p l.
+Inductive atomic_formulae : Prop :=
+  | Afpred: predicative_symbol -> list term -> atomic_formulae.
 
-Check (afpred (predicate "P") [tvar a]).
-Definition phi := atomic_formulae (predicate "p") [tvar a].
-Check (phi /\ phi).
+Check (Afpred (Predicate "P") [Tvar a]).
+Definition phi := Afpred (Predicate "p") [Tvar a].
 
 
 Inductive first_order_formulae : Type :=
-  | Aformula (p: predicative_symbol)(l: list term)
+  | Aformula (phi : atomic_formulae)
   | Anot (phi : first_order_formulae)
   | Aand (phi1 phi2 : first_order_formulae)
   | Aor (phi1 phi2 : first_order_formulae)
@@ -41,32 +40,3 @@ Inductive first_order_formulae : Type :=
   | Aforall (x : var) (phi : first_order_formulae)
   | Aexists (x : var) (phi : first_order_formulae).
 
-Fixpoint first_order_formulae_eval (phi: first_order_formulae) : Prop :=
-  match phi with
-  | Aformula p l => atomic_formulae p l
-  | Anot phi0 => ~(first_order_formulae_eval phi0)
-  | Aand phi1 phi2 => (first_order_formulae_eval phi1) /\ (first_order_formulae_eval phi2)
-  | Aor phi1 phi2 => (first_order_formulae_eval phi1) \/ (first_order_formulae_eval phi2)
-  | Aimplies phi1 phi2 => (first_order_formulae_eval phi1) -> (first_order_formulae_eval phi2)
-  | Adoubleimplies phi1 phi2 => (first_order_formulae_eval phi1) <-> (first_order_formulae_eval phi2)
-  | Aforall x phi0 => forall x: var, (first_order_formulae_eval phi0)
-  | Aexists x phi0 => exists x: var, (first_order_formulae_eval phi0)
-  end.
-
-(* Run some tests *)
-
-Definition P := predicate "P".
-Check(Aformula P [tvar a]).
-Definition phi1 := Aformula P [tvar a].
-Definition phi2 := Aformula P [tvar b; tvar b].
-Check(Aand phi1 phi2).
-Check(Aexists a phi1).
-
-Compute first_order_formulae_eval phi1.
-Compute first_order_formulae_eval (Anot phi1).
-Compute first_order_formulae_eval (Aand phi1 phi2).
-Compute first_order_formulae_eval (Aor phi1 phi2).
-Compute first_order_formulae_eval (Aimplies phi1 phi2).
-Compute first_order_formulae_eval (Adoubleimplies phi1 phi2).
-Compute first_order_formulae_eval (Aforall a phi1).
-Compute first_order_formulae_eval (Aexists b phi2).
